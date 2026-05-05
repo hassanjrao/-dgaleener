1bc00307f17e5cbc86aa4c199abe96599a3c1d14
Hassan <hassanjrao@gmail.com>
2026-05-05 16:53:50 +0500

    composer updated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -28,64 +28,66 @@
 
                 <form method="POST" action="{{ route('register') }}">
                     @csrf
-                    <div class="form-group mb-3">
-                        <label for="type" class="modern-auth-label">User Type</label>
-                        <select class="form-control modern-auth-input @error('type') is-invalid @enderror" id="type" name="type" required>
-                            @foreach (\Spatie\Permission\Models\Role::orderBy('name', 'desc')->whereIn('name', ['practitioner', 'therapist'])->get() as $role)
-                                @if ($role->name != 'administrator')
-                                    <option value="{{ $role->name }}">{{ ucfirst($role->name) }}</option>
-                                @endif
-                            @endforeach
-                        </select>
-                        @error('type')
-                            <span class="invalid-feedback d-block"><strong>{{ $errors->first('type') }}</strong></span>
-                        @enderror
-                    </div>
+                    <div class="row">
+                        <div class="col-12 mb-3">
+                            <label for="type" class="modern-auth-label">User Type</label>
+                            <select class="form-control modern-auth-input @error('type') is-invalid @enderror" id="type" name="type" required>
+                                @foreach (\Spatie\Permission\Models\Role::orderBy('name', 'desc')->whereIn('name', ['practitioner', 'therapist'])->get() as $role)
+                                    @if ($role->name != 'administrator')
+                                        <option value="{{ $role->name }}">{{ ucfirst($role->name) }}</option>
+                                    @endif
+                                @endforeach
+                            </select>
+                            @error('type')
+                                <span class="invalid-feedback d-block"><strong>{{ $errors->first('type') }}</strong></span>
+                            @enderror
+                        </div>
 
-                    <div class="form-group mb-3">
-                        <label for="name" class="modern-auth-label">Name</label>
-                        <input type="text" class="form-control modern-auth-input @error('name') is-invalid @enderror"
-                            value="{{ old('name') }}" name="name" id="name" required>
-                        @error('name')
-                            <span class="invalid-feedback d-block"><strong>{{ $errors->first('name') }}</strong></span>
-                        @enderror
-                    </div>
+                        <div class="col-sm-6 mb-3">
+                            <label for="name" class="modern-auth-label">Name</label>
+                            <input type="text" class="form-control modern-auth-input @error('name') is-invalid @enderror"
+                                value="{{ old('name') }}" name="name" id="name" required>
+                            @error('name')
+                                <span class="invalid-feedback d-block"><strong>{{ $errors->first('name') }}</strong></span>
+                            @enderror
+                        </div>
 
-                    <div class="form-group mb-3">
-                        <label for="username" class="modern-auth-label">Username</label>
-                        <input type="text" class="form-control modern-auth-input @error('username') is-invalid @enderror"
-                            value="{{ old('username') }}" name="username" id="username" required>
-                        @error('username')
-                            <span class="invalid-feedback d-block"><strong>{{ $errors->first('username') }}</strong></span>
-                        @enderror
-                    </div>
+                        <div class="col-sm-6 mb-3">
+                            <label for="username" class="modern-auth-label">Username</label>
+                            <input type="text" class="form-control modern-auth-input @error('username') is-invalid @enderror"
+                                value="{{ old('username') }}" name="username" id="username" required>
+                            @error('username')
+                                <span class="invalid-feedback d-block"><strong>{{ $errors->first('username') }}</strong></span>
+                            @enderror
+                        </div>
 
-                    <div class="form-group mb-3">
-                        <label for="email" class="modern-auth-label">Email</label>
-                        <input type="email" class="form-control modern-auth-input @error('email') is-invalid @enderror"
-                            value="{{ old('email') }}" name="email" id="email" required>
-                        @error('email')
-                            <span class="invalid-feedback d-block"><strong>{{ $errors->first('email') }}</strong></span>
-                        @enderror
-                    </div>
+                        <div class="col-12 mb-3">
+                            <label for="email" class="modern-auth-label">Email</label>
+                            <input type="email" class="form-control modern-auth-input @error('email') is-invalid @enderror"
+                                value="{{ old('email') }}" name="email" id="email" required>
+                            @error('email')
+                                <span class="invalid-feedback d-block"><strong>{{ $errors->first('email') }}</strong></span>
+                            @enderror
+                        </div>
 
-                    <div class="form-group mb-3">
-                        <label for="password" class="modern-auth-label">Password</label>
-                        <input type="password" class="form-control modern-auth-input @error('password') is-invalid @enderror"
-                            name="password" id="password" required>
-                        @error('password')
-                            <span class="invalid-feedback d-block"><strong>{{ $errors->first('password') }}</strong></span>
-                        @enderror
-                    </div>
+                        <div class="col-sm-6 mb-3">
+                            <label for="password" class="modern-auth-label">Password</label>
+                            <input type="password" class="form-control modern-auth-input @error('password') is-invalid @enderror"
+                                name="password" id="password" required>
+                            @error('password')
+                                <span class="invalid-feedback d-block"><strong>{{ $errors->first('password') }}</strong></span>
+                            @enderror
+                        </div>
 
-                    <div class="form-group mb-3">
-                        <label for="password_confirmation" class="modern-auth-label">Confirm Password</label>
-                        <input type="password"
-                            class="form-control modern-auth-input @error('password_confirmation') is-invalid @enderror"
-                            name="password_confirmation" id="password_confirmation" required>
-                        @error('password_confirmation')
-                            <span class="invalid-feedback d-block"><strong>{{ $errors->first('password_confirmation') }}</strong></span>
-                        @enderror
+                        <div class="col-sm-6 mb-3">
+                            <label for="password_confirmation" class="modern-auth-label">Confirm Password</label>
+                            <input type="password"
+                                class="form-control modern-auth-input @error('password_confirmation') is-invalid @enderror"
+                                name="password_confirmation" id="password_confirmation" required>
+                            @error('password_confirmation')
+                                <span class="invalid-feedback d-block"><strong>{{ $errors->first('password_confirmation') }}</strong></span>
+                            @enderror
+                        </div>
                     </div>
 
                     <button type="submit" class="btn modern-auth-btn-main btn-block">{{ __('Sign Up') }}</button>
