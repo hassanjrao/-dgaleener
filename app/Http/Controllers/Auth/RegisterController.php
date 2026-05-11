a5c84217e0019e990e43e2a0cce46044aab38cce
Hassan <hassanjrao@gmail.com>
2026-05-11 19:14:19 +0500

    added password update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'type'    => 'required|in:practitioner,therapist',
+            'type'    => 'required|in:student/estudiante,practitioner/practicante,guest/invitado',
             'name'    => 'required|string|max:255|unique:users',
             'username'=> 'required|string|min:8|unique:users',
             'email'   => 'required|string|email|max:255|unique:users,email',
