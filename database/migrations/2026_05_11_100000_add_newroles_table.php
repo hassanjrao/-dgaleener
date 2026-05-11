dd7486437da84bc880e16003549fdcfbb87d3568
Hassan <hassanjrao@gmail.com>
2026-05-11 19:11:09 +0500

    added password update

--- database/migrations/2026_05_11_100000_add_newroles_table.php
+++ database/migrations/2026_05_11_100000_add_newroles_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-class AddNewRolesTable extends Migration
+class CreateContactMessagesTableCopy extends Migration
 {
     public function up()
     {
